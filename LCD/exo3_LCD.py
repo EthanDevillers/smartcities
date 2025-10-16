77365fb563af85a287097ad884636d09ed2357b5
hepl-DevillersEthan <ethan.devillers@student.hepl.be>
2025-10-16 17:27:03 +0200

    version avec le dimmer de la led

--- LCD/exo3_LCD.py
+++ LCD/exo3_LCD.py
@@ -6,8 +6,8 @@ from dht20 import DHT20
 A4 = 440
 C5 = 523
 E5 = 659
-
-LED = Pin(18, Pin.OUT)
+led_pwm = PWM(Pin(18))
+led_pwm.freq(1000)
 pot = ADC(0)
 buzzer = PWM(Pin(27))
 buzzer.freq(440)
@@ -31,11 +31,25 @@ while True:
         lcd.print("Set: {:.1f}C".format(set_temp))
         lcd.setCursor(0,1)
         lcd.print("Ambient: {:.1f}C".format(ambient))
-        if LED.value() == 0:
-            LED.value(1)
-        else:
-            LED.value(0)    
-        sleep(1)
+        steps = 40
+        pause = 0.0125   # steps * pause * 2 ~= 1s
+        step = 65535 // steps
+        # montée
+        dutycycle = 0
+        for _ in range(steps):
+            led_pwm.duty_u16(dutycycle)
+            dutycycle += step
+            if dutycycle > 65535:
+                dutycycle = 65535
+            sleep(pause)
+        # descente
+        for _ in range(steps):
+            led_pwm.duty_u16(dutycycle)
+            dutycycle -= step
+            if dutycycle < 0:
+                dutycycle = 0
+            sleep(pause)
+        led_pwm.duty_u16(0)
 
         
     elif ambient is not None and ambient > set_temp + 3:
@@ -44,19 +58,19 @@ while True:
         lcd.print("ALARM")
         lcd.setCursor(0,1)
         lcd.print("Set:{:.1f} Amb:{:.1f}".format(set_temp, ambient))
-        LED.value(1)
+        led_pwm.duty_u16(65535)
         buzzer.freq(A4)
         buzzer.duty_u16(32768)
         sleep(0.25)
-        LED.value(0)
+        led_pwm.duty_u16(0)
         buzzer.freq(C5)
         buzzer.duty_u16(32768)
         sleep(0.25)
-        LED.value(1)
+        led_pwm.duty_u16(65535)
         buzzer.freq(E5)
         buzzer.duty_u16(32768)
         sleep(0.25)
-        LED.value(0)
+        led_pwm.duty_u16(0)
         buzzer.duty_u16(0)
         sleep(0.25) 
  
